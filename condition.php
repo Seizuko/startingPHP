e7162ade27aaafc4b7e007399b16a21a269539b0
Ansion Quentin <ansion.quentin@outlook.fr>
2017-03-07 16:59:57 +0100

    Add files via upload

--- condition.php
+++ condition.php
@@ -42,7 +42,7 @@
 		<div class="col-xs-12 col-sm-6 col-md-2 col-lg-2" id="border">
 			<p>exercice 3</p>
 			<?php 
-				$age = 12;
+				$age = 21;
 				$genre = 'homme';
 					if ($genre == 'homme') {
 						echo 'Vous êtes un homme';
@@ -57,9 +57,34 @@
 				}				
 			?>
 		</div>
+		<div class="col-xs-12 col-sm-6 col-md-2 col-lg-2" id="border">
+			<p>exercice 3</p>
+			<?php 
+			// Désolé julien le else if c'est plus simple ! //
+				$magnitude = 6;
+					if ($magnitude == 1) {
+						echo 'Micro-séisme impossible à ressentir.';
+					}else if($magnitude == 2){
+						echo 'Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.';
+					}else if($magnitude == 3){
+						echo 'Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.';
+					}else if($magnitude == 4){
+						echo 'Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.';
+					}else if($magnitude == 5){
+						echo 'Séisme capable d\'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.';
+					}else if($magnitude == 6){
+						echo 'Fort séisme capable d\'engendrer des destructions majeures sur une large distance (180 km) autour de l\'épicentre.';
+					}else if($magnitude == 7){
+						echo 'Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.';
+					}else if($magnitude == 8){
+						echo 'Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.';
+					}else if($magnitude == 9){
+						echo 'Séisme capable de tout détruire sur une très vaste zone.';
+					}
 
-
-
+							
+			?>
+		</div>   
 	</div>
 </div>
 
