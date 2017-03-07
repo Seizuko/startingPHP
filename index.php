897d73fcbdfd1f8d21e498393aba3a4f300566f5
Ansion Quentin <ansion.quentin@outlook.fr>
2017-03-07 13:59:17 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exo php</title>
+</head>
+<body>
+
+<a href="variable.php">exo 1 (variable.php)</a>
+
+
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<p style="color:red;">exercice 1</p>
+<?php 
+	$nom = 'jean';
+	echo $nom;
+?>
+
+<br><br><p style="color:red;">exercice 2</p>
+
+<?php
+	$nom = 'jean';
+	$prenom = 'valjean';
+	$age = '42';
+
+	echo 'nom : '.$nom.'<br> prenom : '.$prenom.' <br>age : '.$age;
+?>
+
+<br><br><p style="color:red;">exercice 3</p>
+
+<?php 
+ $kilometre = 1;
+ 	echo 'kilometre : '.$kilometre.'<br>';
+
+ $kilometre = 3;
+ 	echo 'kilometre : '.$kilometre.'<br>';
+
+ $kilometre = 125;
+ 	echo 'kilometre : '.$kilometre.'<br>';
+
+?>
+</body>
+</html>
